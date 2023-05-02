c829189e4a819d4197c8edbacbb4d5bfa90aa312
Towhid-Raiyan <towhid.raiyan.com>
2023-05-03 03:40:20 +0600

    json file send completed

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const data = require('./data/data.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Dragon is running!')
+})
+
+app.get('/data',(req,res)=>{
+    res.send(data);
 })
 
 app.listen(port, () => {
